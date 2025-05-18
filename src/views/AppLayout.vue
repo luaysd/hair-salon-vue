616a31a30eab9103bdc34f45cff64fb2b98a9369
Luay Sarieddine <Luay.Sarieddine@capital-banking.com>
2025-05-18 16:12:00 +0300

    test 4 working

--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -13,7 +13,6 @@
     </main>
     
     <footer>
-      test
       <p style="font-size: 23px;">Developed By <a href="http://luaysd.oncloudlb.com" target="_blank" rel="noopener noreferrer" class="link">Luay Sariedeen</a> </p>
     </footer>
   </div>
